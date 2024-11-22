4e0c1cb16df7a7c8dfbbe586f6bfe71e0d8083ba
Bas Nijholt <bas@nijho.lt>
2024-11-22 10:32:08 -0800

    add `src/index.js`

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import jQuery from "jquery";
+window.jQuery = window.$ = jQuery;
+
+import GraphvizSvg from "./graphvizsvg";
+import "bootstrap";
+import "./plugin";
+
+export default GraphvizSvg;
